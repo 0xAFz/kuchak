cd208c79b1fcc72ba97b506ad0b2d7eb14339a75
AFz <0xafz@proton.me>
2024-10-20 16:34:54 +0330

    feat: add email verification with redis

--- internal/repository/account_redis.go
+++ internal/repository/account_redis.go
@@ -2,7 +2,8 @@ package repository
 
 import (
 	"context"
-	"kuchak/internal/entity"
+	"fmt"
+	"time"
 
 	"github.com/redis/rueidis"
 )
@@ -17,10 +18,29 @@ func NewAccountRedisRepository(redisClient rueidis.Client) *AccountRedisReposito
 	return &AccountRedisRepository{client: redisClient}
 }
 
-func (a *AccountRedisRepository) SaveVerifyEmail(ctx context.Context, email, token string) error {
-	panic("...")
+func (a *AccountRedisRepository) SaveVerifyToken(ctx context.Context, email, token string, ttl time.Duration) error {
+	key := "verify:" + token
+	cmd := a.client.B().Set().Key(key).Value(email).Nx().Px(ttl).Build()
+
+	err := a.client.Do(ctx, cmd).Error()
+	if err != nil {
+		return fmt.Errorf("failed to set token into redis")
+	}
+
+	return nil
 }
 
-func (a *AccountRedisRepository) ByToken(ctx context.Context, token string) entity.VerifyEmail {
-	panic("...")
+func (a *AccountRedisRepository) ByToken(ctx context.Context, token string) (string, error) {
+	key := "verify:" + token
+	cmd := a.client.B().Getdel().Key(key).Build()
+
+	result, err := a.client.Do(ctx, cmd).ToString()
+	if err != nil {
+		if rueidis.IsRedisNil(err) {
+			return "", fmt.Errorf("token not found or expired")
+		}
+		return "", fmt.Errorf("failed to get token from redis: %w", err)
+	}
+
+	return result, nil
 }
